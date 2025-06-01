2be50327085eb9c4b0fb9a4a025649b5a17db8e9
ngergihun <ngergihun@gmail.com>
2025-06-01 09:19:44 +0200

    Measurement start works, inital parameter cast bug still there

--- ScannerApp.py
+++ ScannerApp.py
@@ -16,27 +16,42 @@ import asyncio
 
 from PySide6 import QtWidgets
 from PySide6 import QtCore, QtGui
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QMessageBox, QWidget, QHBoxLayout, QFormLayout, QLineEdit, QSpinBox
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QMessageBox, QWidget, QHBoxLayout, QVBoxLayout, QFormLayout, QLineEdit, QSpinBox, QPushButton
 from PySide6.QtCore import QTimer, QObject, QThread, Signal, Slot
 
+import gui
 from gui import LineEdit, ScanEditor
+
+import numpy as np
+import logging
+
+logger = logging.getLogger('logger')
+logger.setLevel(logging.DEBUG)
+if not logger.hasHandlers():
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+logger.info('Starting AutoScanApp')
 
 # import neaSDK
 try:
     import nea_tools
     offline_mode = False
 except:
-    print("nea_tools module not found, working in offline mode")
+    logger.warning("nea_tools module not found, working in offline mode")
     offline_mode = True
 
 class neaSNOM():
     def __init__(self,path_to_dll,fingerprint):
+
         self.name = None
         self.connected = False
         self.context = None
         self.nea = None
+        self.scan_parameters = None
 
-    def connect_to_sensor(self,path_to_dll,fingerprint):
+    def connect(self,path_to_dll,fingerprint):
         if "nea_tools" not in sys.modules:
             print("nea_tools module was not found, missing SDK!")
             return False
@@ -53,76 +68,172 @@ class neaSNOM():
         try:
             from neaspec import context
             import Nea.Client.SharedDefinitions as nea
+
+            # global nea_tools
+            # import nea_tools
+
+            global scan
+            from nea_tools.logic import scan
+            # from nea_tools import database, plotter
+            # from nea_tools.utils.colors import COLORS,CMAPS
+            global approach_sample
+            from nea_tools.logic.approach import approach_sample
+
         except ModuleNotFoundError:
             raise ConnectionError('Connection refused or timeout. Retry to connect again.')
         else:
             self.connected = True
-            print('\nConnected to SNOM.')
 
             self.context = context
             self.nea = nea
 
             return True
         
-    def get_power(self):
-        return self.context.Microscope.Py.EnergySensor
-    
+    def spawn_parameters(self):
+        if self.connected:
+            self.scan_parameters = self.context.Logic.DefaultScanParameters.Spawn()
+
     def close(self):
         if self.connected:
-            print('\nDisconnecting from neaServer!')
+            logger.debug('\nDisconnecting from neaServer!')
             nea_tools.disconnect()
             self.connected = False
         else:
-            print("SNOM was not connected!")
+            logger.debug("SNOM was not connected!")
 
 ## Worker class to handle computaionally heavy tasks in a separate thread
 class Worker(QObject):
+
     progress = Signal(str)
     finished = Signal()
     error = Signal(str)
 
-    def __init__(self):
+    parameters = {}
+
+    def __init__(self, snom):
         super().__init__()
+
+        self.snom = snom
+        self.newscan = None
+
+    def print_params(self):
+        logger.info("Current parameters: %s", self.parameters)
+
+    def create_measurement(self):
+        if self.parameters is not None:
+            try:
+                p = self.snom.context.Logic.DefaultScanParameters.Spawn()
+                return scan.Whitelight(Name = "Test Scan", 
+                                PhysicalOffsetX=self.parameters["scan"]["PhysicalOffsetX"], 
+                                PhysicalOffsetY=self.parameters["scan"]["PhysicalOffsetY"],
+                                PhysicalSizeX=self.parameters["scan"]["PhysicalSizeX"],
+                                PhysicalSizeY=self.parameters["scan"]["PhysicalSizeY"],
+                                TargetResolutionWidth=self.parameters["scan"]["TargetResolutionWidth"],
+                                TargetResolutionHeight=self.parameters["scan"]["TargetResolutionHeight"],
+                                Angle=self.parameters["scan"]["Angle"], 
+                                TargetMillisecondsPerPixel=self.parameters["scan"]["TargetMillisecondsPerPixel"],
+                                LaserSourceTargetWavelength=p.LaserSourceTargetWavelength,
+                                PhysicalRangeM=(665.5,665.5))
+            except Exception as e:
+                logger.error("Error creating scan object: %s", e)
+                return None
+        else:
+            logger.error("Could not configure scan object.")
+            return None
+
+    def run_measurement(self):
+        logger.info("Starting measurement with parameters: %s", self.parameters)
+        newscan = self.create_measurement()
+        if newscan is not None:
+            approach_sample(0.8)
+            with newscan as wl:
+                wl.scan()
+                logger.debug("Measurement started, waiting for scan to finish...")
+                wl.wait_for_scan()
+                logger.debug("Scan finished, downloading data...")
+                data = wl.data[f"M1A"]
+                logger.info(f"Data type: {type(data)}, Data shape: {np.shape(data)}")
+                np.savetxt("TestSave", data)
 
 class AutoScanApp(QMainWindow):
 
-    start_zero_adjust = Signal()
-
-    offline_mode = True
+    offline_mode = False
 
     def __init__(self):
         super().__init__()
-
-         # Set up UI
-        self.setup_ui()
         
         self.snom_connected = False
         self.config = None
         self.settings = None
         self.read_config()
-        self.read_settings()
         
         # Create the worker thread
-        self.worker = Worker()
+        self.worker = Worker(snom=None)
         self.worker_thread = QThread()
         self.worker.moveToThread(self.worker_thread)
         self.worker_thread.start()
-        
 
+        # Set up UI
+        self.setup_ui()
+        
     def setup_ui(self):
 
-        self.setWindowTitle("My App")
+        self.setWindowTitle("Auto Step Scanner App")
 
-        mainlayout = QHBoxLayout()
+        mainlayout = QVBoxLayout()
         mainlayout.setContentsMargins(0,0,0,0)
-        mainlayout.setSpacing(20)
+        # mainlayout.setSpacing(10)
 
-        editor = ScanEditor(self)
-        mainlayout.addWidget(editor)
+        self.scan_editor = ScanEditor(self)
+        self.ifg_editor = gui.InterferometerEditor(self)
+        self.info = gui.InfoDisplay(self)
+
+        self.connect_widget = QWidget()
+        self.connect_widget.setLayout(QHBoxLayout())
+        self.connect_button = QPushButton("Connect")
+        self.connect_led = gui.LedIndicator()
+        self.connect_widget.layout().addWidget(self.connect_button)
+        self.connect_widget.layout().addWidget(self.connect_led)
+        self.connect_button.clicked.connect(self.connect_snom)
+
+        self.start_measurement_button = QPushButton("Start Measurement")
+        self.start_measurement_button.clicked.connect(self.worker.run_measurement)
+        self.start_measurement_button.setEnabled(False)
+
+        self.ifg_editor.edited.connect(self.on_parameters_changed)
+        self.ifg_editor.edited.emit(self.ifg_editor.parameters)
+
+        self.scan_editor.edited.connect(self.on_parameters_changed)
+        self.scan_editor.edited.emit(self.scan_editor.parameters)
+
+        mainlayout.addWidget(self.scan_editor)
+        mainlayout.addWidget(self.ifg_editor)
+        mainlayout.addWidget(self.info)
+        mainlayout.addWidget(self.connect_widget)
+        mainlayout.addWidget(self.start_measurement_button)
 
         container = QWidget()
         container.setLayout(mainlayout)
         self.setCentralWidget(container)
+
+    def on_parameters_changed(self):
+        self.set_info_display()
+        self.send_parameters_to_worker()
+
+        logger.debug("Parameters changed, info display and worker parameters updated")
+
+    def set_info_display(self):
+        self.info.set_scan_parameters(self.scan_editor.parameters)
+        self.info.set_ifg_parameters(self.ifg_editor.parameters)
+
+        logger.debug("Info display updated with current parameters")
+
+    def send_parameters_to_worker(self):
+        self.worker.parameters = {
+            'scan': self.scan_editor.parameters,
+            'ifg': self.ifg_editor.parameters
+        }
+        logger.info("Parameters sent to worker")
 
     def check_snom_config(self):
         if (self.config['fingerprint'] == 'CHANGEMEE') or (self.config['path_to_dll'] == r"CHANGEMEE"):
@@ -146,17 +257,65 @@ class AutoScanApp(QMainWindow):
 
     def read_settings(self):
         with open('settings.yaml', 'r') as file:
-            self.settings = yaml.safe_load(file)
+            all_settings = yaml.safe_load(file)
+            print(all_settings)
 
     def write_settings(self):
         with open('settings.yaml', 'w') as file:
-            yaml.dump(self.settings, file)
+            yaml.dump([self.scan_editor.parameters, self.ifg_editor.parameters], file)
 
-    def connect_sensor(self):
-        objname = globals()[self.sensor_combobox.currentText()]
-        self.powersensor = objname()
-        self.worker.sensor = self.powersensor
-        self.sensor_control_frame.setEnabled(True)
+    def connect_snom(self):
+        self.check_snom_config()
+        if self.offline_mode:
+            logger.warning("Working in offline mode, no SNOM connection")
+            return
+        else:
+            if not self.snom_connected:
+                if self.worker.snom is None:
+                    self.worker.snom = neaSNOM(self.config['path_to_dll'], self.config['fingerprint'])
+                
+                if self.worker.snom.connect(self.config['path_to_dll'], self.config['fingerprint']):
+                    if self.worker.snom.connected == True:
+                        self.snom_connected = True
+                        self.connect_led.setChecked(True)
+                        self.connect_button.setText("Disconnect")
+                        logger.info("Connected to SNOM")
+                else:
+                    logger.error("Failed to connect to SNOM")
+                    self.connect_led.setChecked(False)
+                    self.connect_button.setText("Connect")
+                    QMessageBox.critical(self, "Connection Error", "Failed to connect to SNOM. Check your configuration.")
+            else:
+                self.worker.snom.close()
+                self.snom_connected = False
+                self.connect_led.setChecked(False)
+                self.connect_button.setText("Connect")
+
+            self.start_measurement_button.setEnabled(self.snom_connected)
+
+
+    def closeEvent(self, event):
+        quit_msg = "Are you sure you want to exit the program?"
+        reply = QtWidgets.QMessageBox.question(self, 'Message', quit_msg, QtWidgets.QMessageBox.Yes, QtWidgets.QMessageBox.No)
+
+        if reply == QtWidgets.QMessageBox.Yes:
+            event.accept()
+
+            if self.worker.snom is not None:
+                try:
+                    self.worker.snom.close()
+                    logger.info("SNOM connection closed before program exit!")
+                except:
+                    logger.error(f'{self.worker.snom} could NOT be closed upon program exit!')
+
+            if self.worker_thread.isRunning():
+                self.worker_thread.quit()
+                self.worker_thread.wait()
+                logger.info("Worker thread closed before program exit!")
+            logger.info("Exiting AutoScanApp! Bye-bye!")
+
+        else:
+            event.ignore()
 
 
 if __name__ == '__main__':
--- gui.py
+++ gui.py
@@ -8,6 +8,20 @@ from PySide6.QtCore import Qt, QPointF, Property
 from PySide6.QtGui import QColor, QPainter, QPen, QBrush, QRadialGradient
 from PySide6.QtWidgets import QAbstractButton
 
+from enum import Enum
+import datetime
+
+import logging
+
+logger = logging.getLogger('logger')
+logger.setLevel(logging.INFO)
+if not logger.hasHandlers():
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+logger.info('Starting GUI')
+
 # Taken from https://github.com/nlamprian/pyqt5-led-indicator-widget
 class LedIndicator(QAbstractButton):
     scaledSize = 1000.0
@@ -23,6 +37,8 @@ class LedIndicator(QAbstractButton):
         self.on_color_2 = QColor(0, 192, 0)
         self.off_color_1 = QColor(0, 28, 0)
         self.off_color_2 = QColor(0, 128, 0)
+
+        self.setDisabled(True)
 
     def resizeEvent(self, QResizeEvent):
         self.update()
@@ -133,8 +149,11 @@ class FloatOrEmptyValidator(AnyOrEmptyValidator):
         self.valid_type = float
         super().__init__(parent, allow_empty=allow_empty, bottom=bottom, top=top,
                          default_text=default_text)
+
 # Copy-modified from orange-spectroscopy
 class LineEdit(QLineEdit):
+    edited = Signal()
+
     def __init__(self, bottom, top, *args, **kwargs):
         super(LineEdit, self).__init__(*args, **kwargs)
         self.validator = FloatOrEmptyValidator(self, allow_empty=False, bottom=bottom, top=top)
@@ -147,6 +166,7 @@ class LineEdit(QLineEdit):
         super().keyPressEvent(event)
         if event.key() == QtCore.Qt.Key_Return and not self.hasAcceptableInput():
             self.check_validator()
+        self.edited.emit()
 
     def new_text(self, text):
         if self.hasAcceptableInput():
@@ -256,7 +276,6 @@ def _addSpace(widget, space):
         else:
             separator(widget)
 
-
 def separator(widget, width=None, height=None):
     """
     Add a separator of the given size into the widget.
@@ -277,7 +296,6 @@ def separator(widget, width=None, height=None):
     sep.setFixedSize(*size)
     return sep
 
-
 def separator_size(width=None, height=None):
     if is_macstyle():
         width = 2 if width is None else width
@@ -286,7 +304,6 @@ def separator_size(width=None, height=None):
         width = 4 if width is None else width
         height = 4 if height is None else height
     return width, height
-
 
 def miscellanea(control, box, parent, *,
                 addToLayout=True, stretch=0, sizePolicy=None,
@@ -374,61 +391,198 @@ def miscellanea(control, box, parent, *,
         _addSpace(parent, addSpaceBefore)
         parent.layout().addWidget(box or control, stretch)
 
-class ScanEditor(QWidget):
-    name = "Rotate phase"
-    qualname = "orangecontrib.snom.phase_rotation_test"
 
-    edited = Signal()
+ScanMode = Enum('ScanMode', [('WLI', 'Whitelight Step Scan'),('WLI_single','Single Whitelight Scan')])
 
-    parameters = {}
+class InterferometerEditor(QWidget):
+
+    edited = Signal(dict)
+    parameters = {"InterferometerCenter": 400.0,
+                  "InterferometerDistance": 800.0,
+                  "NumberOfPoints": 600,
+                  "NumberOfSkippedPoints": 0,
+                  "StartPosition": 800.0,
+                  "EndPosition": 0.0}
 
     def __init__(self, parent=None, **kwargs):
         super().__init__(parent, **kwargs)
 
-        default_center = 50.0
+        self.setLayout(QVBoxLayout())
+        self.form = QFormLayout()
+        self.basebox = widgetBox(self, "", orientation=self.form)
+
+        self.basebox = widgetBox(self, f"White Light Step Scan Parameters", orientation=self.form)
+        self.le1 = LineEdit(bottom=0.0, top=800.0, text="400.0")
+        self.form.addRow("Interferometer Center:", self.le1)
+        self.le2 = LineEdit(bottom=0.0, top=800.0, text="800.0")
+        self.form.addRow("Interferometer Distance:", self.le2)
+
+        self.sp1 = QSpinBox()
+        self.sp1.setRange(1, 10000)
+        self.sp1.setValue(600)
+        self.form.addRow("Number of points:", self.sp1)
+
+        self.cast_default_values()
+        self.connect_signals()
+        self.edited.emit(self.parameters)
+
+    def cast_default_values(self):
+        self.le1.setText(str(self.parameters["InterferometerCenter"]))
+        self.le2.setText(str(self.parameters["InterferometerDistance"]))
+        self.sp1.setValue(self.parameters["NumberOfPoints"])
+
+    def connect_signals(self):
+        self.le1.edited.connect(self.set_parameters)
+        self.le2.edited.connect(self.set_parameters)
+        self.sp1.valueChanged.connect(self.set_parameters)
+
+    def set_parameters(self):
+        self.parameters["InterferometerCenter"] = float(self.le1.text())
+        self.parameters["InterferometerDistance"] = float(self.le2.text())
+        self.parameters["NumberOfPoints"] = int(self.sp1.value())
+        self.parameters["NumberOfSkippedPoints"] = 0
+        self.parameters["StartPosition"] = 800.0
+        self.parameters["EndPosition"] = 0.0
+
+        logger.debug(self.parameters)
+        self.edited.emit(self.parameters)
+
+class InfoDisplay(QWidget):
+
+    interferogram_parameters = None
+    scan_parameters = None
+    
+    def __init__(self, parent=None, **kwargs):
+        super().__init__(parent, **kwargs)
+
+        self.setLayout(QVBoxLayout())
+        self.boxlayout = QVBoxLayout()
+        self.basebox = widgetBox(self, "Info", orientation=self.boxlayout)
+
+        self.line1 = QLabel("Estimated time: 0:00:00")
+        self.basebox.layout().addWidget(self.line1)
+
+    def set_scan_parameters(self, scan_parameters):
+        self.scan_parameters = scan_parameters
+        self.update_info()
+
+    def set_ifg_parameters(self, interferogram_parameters):
+        self.interferogram_parameters = interferogram_parameters
+        self.update_info()
+
+    def update_info(self):
+        self.calculate_time()
+
+    def calculate_time(self):
+        if self.scan_parameters is not None and self.interferogram_parameters is not None:
+            try:
+                nop = self.scan_parameters["TargetResolutionWidth"] * self.scan_parameters["TargetResolutionHeight"] * self.interferogram_parameters["NumberOfPoints"]
+                time_per_pixel = self.scan_parameters["TargetMillisecondsPerPixel"] / 1000.0
+                total_time = nop * time_per_pixel * 2 # Multiply by 2 for forward and backward scan
+                self.line1.setText("Estimated time: " + str(datetime.timedelta(seconds=total_time)))
+            except KeyError:
+                self.line1.setText("Estimated time: Unknown")
+
+
+class ScanEditor(QWidget):
+
+    edited = Signal(dict)
+
+    parameters = {"PhysicalOffsetX": 50.0,
+                  "PhysicalOffsetY": 50.0,
+                  "PhysicalSizeX": 100,
+                  "PhysicalSizeY": 100,
+                  "TargetResolutionWidth": 100,
+                  "TargetResolutionHeight": 100,
+                  "Angle": 0.0,
+                  "TargetMillisecondsPerPixel": 9.8}
+
+    def __init__(self, parent=None, **kwargs):
+        super().__init__(parent, **kwargs)
 
         layout = QVBoxLayout()
         self.setLayout(layout)
+
+        # General settings for the scan
         form = QFormLayout()
         box = widgetBox(self, "Basic settings", orientation=form)
-
+        # For scan mode selection
+        self.mode_selector = QComboBox()
+        self.mode_selector.addItems(["Whitelight Step Scan"])
+        form.addRow("Scan Mode", self.mode_selector)
         # For center position of the scanner
-        cxedit = LineEdit(bottom=0.0, top=100.0)
-        cxedit.setText(str(default_center))
-        cyedit = LineEdit(bottom=0.0, top=100.0)
-        cyedit.setText(str(default_center))
+        self.cxedit = LineEdit(bottom=0.0, top=100.0)
+        self.cyedit = LineEdit(bottom=0.0, top=100.0)
         scanner_center_widget = QWidget()
         scanner_center_widget.setLayout(QHBoxLayout())
-        scanner_center_widget.layout().addWidget(cxedit)
+        scanner_center_widget.layout().addWidget(self.cxedit)
         scanner_center_widget.layout().addWidget(QLabel("x"))
-        scanner_center_widget.layout().addWidget(cyedit)
-
+        scanner_center_widget.layout().addWidget(self.cyedit)
         form.addRow("Scanner Center Position", scanner_center_widget)
-
         # For scan area
-        axedit = LineEdit(bottom=0.0, top=100.0)
-        axedit.setText(str(1.0))
-        ayedit = LineEdit(bottom=0.0, top=100.0)
-        ayedit.setText(str(1.0))
+        self.axedit = LineEdit(bottom=0.0, top=100.0)
+        self.ayedit = LineEdit(bottom=0.0, top=100.0)
         scan_area_widget = QWidget()
         scan_area_widget.setLayout(QHBoxLayout())
-        scan_area_widget.layout().addWidget(axedit)
+        scan_area_widget.layout().addWidget(self.axedit)
         scan_area_widget.layout().addWidget(QLabel("x"))
-        scan_area_widget.layout().addWidget(ayedit)
-
+        scan_area_widget.layout().addWidget(self.ayedit)
         form.addRow("Scan Area", scan_area_widget)
-
         # For pixel area
-        pxedit = QSpinBox()
-        pxedit.setRange(1, 10000)
-        pxedit.setValue(100)
-        pyedit = QSpinBox()
-        pyedit.setRange(1, 10000)
-        pyedit.setValue(100)
+        self.pxedit = QSpinBox()
+        self.pxedit.setRange(1, 10000)
+        self.pyedit = QSpinBox()
+        self.pyedit.setRange(1, 10000)
         pixel_area_widget = QWidget()
         pixel_area_widget.setLayout(QHBoxLayout())
-        pixel_area_widget.layout().addWidget(pxedit)
+        pixel_area_widget.layout().addWidget(self.pxedit)
         pixel_area_widget.layout().addWidget(QLabel("x"))
-        pixel_area_widget.layout().addWidget(pyedit)
-
+        pixel_area_widget.layout().addWidget(self.pyedit)
         form.addRow("Pixel Area", pixel_area_widget)
+        # For rotation
+        self.rotedit = LineEdit(bottom=0.0, top=360.0)
+        form.addRow("Rotation", self.rotedit)
+        # For integration time
+        self.timeedit = LineEdit(bottom=0.4, top=1000.4)
+        form.addRow("Integration Time (ms)", self.timeedit)
+
+        self.cast_default_values()
+        self.connect_signals()
+        self.edited.emit(self.parameters)
+
+    def connect_signals(self):
+        self.timeedit.edited.connect(self.set_parameters)
+        self.cxedit.edited.connect(self.set_parameters)
+        self.cyedit.edited.connect(self.set_parameters)
+        self.axedit.edited.connect(self.set_parameters)
+        self.ayedit.edited.connect(self.set_parameters)
+        self.pxedit.valueChanged.connect(self.set_parameters)
+        self.pyedit.valueChanged.connect(self.set_parameters)
+
+    def cast_default_values(self):
+        # Scanner center position
+        self.cxedit.setText(str(50.0))
+        self.cyedit.setText(str(50.0))
+        # Scan area
+        self.axedit.setText(str(1.0))
+        self.ayedit.setText(str(1.0))
+        # Pixel area
+        self.pxedit.setValue(100)
+        self.pyedit.setValue(100)
+        # Rotation
+        self.rotedit.setText(str(0.0))
+        # Integration time
+        self.timeedit.setText(str(9.8))
+
+    def set_parameters(self):
+        self.parameters["PhysicalOffsetX"] = float(self.cxedit.text())
+        self.parameters["PhysicalOffsetY"] = float(self.cyedit.text())
+        self.parameters["PhysicalSizeX"] = float(self.axedit.text())
+        self.parameters["PhysicalSizeY"] = float(self.ayedit.text())
+        self.parameters["TargetResolutionWidth"] = int(self.pxedit.value())
+        self.parameters["TargetResolutionHeight"] = int(self.pyedit.value())
+        self.parameters["Angle"] = float(self.rotedit.text())
+        self.parameters["TargetMillisecondsPerPixel"] = float(self.timeedit.text())
+
+        logger.debug("Scan parameters set: %s", self.parameters)
+        self.edited.emit(self.parameters)
